de6e8f3bf0ba57408138e40f9fc108a6b9e89e3e
skypotato <hunsooski@naver.com>
2021-02-18 22:18:09 +0900

    2021.02.18 : 새로운 할인 정책 적용과 문제점 #3

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
@@ -10,7 +11,8 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
     private MemberRepository memberRepository = new MemoryMemberRepository();
-    private DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    //    private DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
--- src/test/java/hello/core/discount/RateDiscountPolicyTest.java
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -2,11 +2,10 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 class RateDiscountPolicyTest {
 
@@ -16,10 +15,21 @@ class RateDiscountPolicyTest {
     @DisplayName("VIP는 10% 할인이 적용되어야 한다.")
     void vip_o(){
         // given
-        Member member = new Member(1L, "memberA", Grade.VIP);
+        Member member = new Member(1L, "memberVIP", Grade.VIP);
         // when
         int discount = rateDiscountPolicy.discount(member, 10000);
         // then
-        Assertions.assertThat(discount).isEqualTo(1000);
+        assertThat(discount).isEqualTo(1000);
+    }
+
+    @Test
+    @DisplayName("VIP가 아닐 경우 할인이 적용되지 않아야 한다.")
+    void vip_x(){
+        // given
+        Member member = new Member(2L, "memberBASIC", Grade.BASIC);
+        // when
+        int discount = rateDiscountPolicy.discount(member, 10000);
+        // then
+        assertThat(discount).isEqualTo(0);
     }
 }
