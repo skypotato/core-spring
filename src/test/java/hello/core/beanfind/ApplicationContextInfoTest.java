4fabc3af191d04e556824451169ad427a7d22df5
skypotato <hunsooski@naver.com>
2021-03-04 21:29:56 +0900

    2021.03.03 : 원하는 빈 출력 방법 #4

--- src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
@@ -25,8 +25,9 @@ public class ApplicationContextInfoTest {
         for (String beanDefinitionName : beanDefinitionNames) {
             BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
 
-            // 내가 둥록한 Bean만 보고 싶을 경우
-            if(beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION){
+            // Role ROLE_APPLICATION : 직접 등록한 애플리케이션 빈
+            // Role ROLE_INFRASTRUCTURE : 스프링 내부에서 사용하는 빈
+            if(beanDefinition.getRole() == BeanDefinition.ROLE_INFRASTRUCTURE){
                 Object bean = ac.getBean(beanDefinitionName);
                 System.out.println("name = " + beanDefinitionName + " object = "+ bean);
             }
