18926ee4596f03c1603e2773f4c47f902cad93ad
skypotato <hunsooski@naver.com>
2021-03-18 20:31:49 +0900

    2021.03.18 : configuation singleton 적용 되는지 테스트   #5

---
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,22 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberServiceImpl;
+import hello.core.order.OrderServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ConfigurationSingletonTest {
+
+    @Test
+    void configurationTest(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+
+        Assertions.assertThat(memberService.getMemberRepository()).isSameAs(orderService.getMemberRepository());
+    }
+}
