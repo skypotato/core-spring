9cb88a92a6a453c0bb7ac10c3fad9815430fd797
skypotato <hunsooski@naver.com>
2021-03-23 19:40:26 +0900

    2021.03.23 : 컴포넌트 스캔 default #6

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -8,7 +8,8 @@ import org.springframework.context.annotation.FilterType;
 @ComponentScan(
         // 탐색할 패키지의 시작위치를 지정함.
         basePackages = "hello.core.member",
-        // 지정한 클래스의 패키지를 탐색 시작 위로 지정함.(default : @ComponentScan이 붙은 설정 정보 클래스의 패키지가 시작위치로 됨)
+        // 지정한 클래스의 패키지를 탐색 시작 위로 지정함.
+        // (Default : @ComponentScan이 붙은 설정 정보 클래스의 패키지가 시작위치로 됨)
         basePackageClasses = AppConfig.class,
         // @Configuration 컴포넌트 스캔에서 제외(예제코드 충돌로 인해 제외)
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
