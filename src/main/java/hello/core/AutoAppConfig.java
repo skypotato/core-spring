d100df076a4baa6c8a256a571c9ae8d71aa6f518
skypotato <hunsooski@naver.com>
2021-03-23 19:17:09 +0900

    2021.03.23 : 컴포넌트 스캔 설정 #6

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        // 탐색할 패키지의 시작위치를 지정함.
+        basePackages = "hello.core.member",
         // @Configuration 컴포넌트 스캔에서 제외(예제코드 충돌로 인해 제외)
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
