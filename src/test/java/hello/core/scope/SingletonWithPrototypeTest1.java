0661cfd600d964b8fe8ecc44fc9148593529c3d8
skypotato <hunsooski@naver.com>
2021-07-19 22:10:13 +0900

    2021.07.19 : 싱글톤에서 프로토 타입 스코프 #9

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -3,6 +3,7 @@ package hello.core.scope;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 import org.springframework.test.context.TestExecutionListeners;
